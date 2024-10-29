69ccaae7a2c337c2158833c63faf6ed7be98b964
Matias Carrillo <132945209+MC6115@users.noreply.github.com>
2024-10-29 18:52:18 -0500

    Update server.js

--- backend-vercel/server.js
+++ backend-vercel/server.js
@@ -3,10 +3,11 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const path = require('path');
 
 require("./database");
 
-const { connectDB, disconnectDB } = require("./db");
+const { connectDB, disconnectDB } = require("./database");
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 
@@ -33,12 +34,10 @@ app.post("/addReview", upload.single('images'), addReview);
 app.get("/seed", seedDatabase);
 app.delete("/removeReview", removeReview);
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
-console.log('We ARE LIVE');
-
 process.on("SIGINT", async () => {
 	await disconnectDB();
 	process.exit(0);
 });
+
 
 
